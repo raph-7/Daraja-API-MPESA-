1d4e22ec6d97e05cb918cb23aaa59d63223f1cb0
raph-7 <raphaelooti@gmail.com>
2025-11-22 14:01:03 +0300

    added query files

--- stkPush.php
+++ stkPush.php
@@ -3,7 +3,7 @@
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://1c95-105-161-14-223.ngrok-free.app/MPEsa-Daraja-Api/callback.php';
+$callbackurl = 'https://www.schoolfit-uniforms.co.ke/darajaApi/callback.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
